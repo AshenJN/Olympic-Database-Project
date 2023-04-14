4bb10bce584701d958504a0dc5bc189b9d261d70
Yifan Zhu <zhujimbo@gmail.com>
2023-04-14 00:34:16 -0400

    Initial commit, template website

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+return (
+	<div>
+	<h1>
+		Welcome to the Olympics Database.
+	</h1>
+    <p style={{fontSize: "14px"}}>
+This is a website that allows users to browse data analytics about the past olympic games
+</p>
+	</div>
+);
+};
+
+export default About;
