7ce99a3f462937a3fe2e9ffe156e2b158b0b96f0
JaimeToranzo <58479770+JaimeToranzo@users.noreply.github.com>
2023-04-18 17:32:47 -0400

    database connection test

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,49 @@
 import React from "react";
-import olym from './images/Olympic.png'
+import olym from "./images/Olympic.png";
 const About = () => {
-return (
-	<div>
-	<h1>
-		Welcome to the Olympics Database.
-	</h1>
-    <p style={{fontSize: "14px"}}>
-This is a website that allows users to browse data analytics about the past olympic games
-</p>
-<img src={olym} width="400" height="200" style={{ marginLeft: "50px" }}/>
-	</div>
-);
+  const oracledb = require("oracledb");
+  const config = {
+    user: "",
+    password: "",
+    connectString: "oracle.cise.ufl.edu/orcl",
+  };
+
+  async function getAthletes() {
+    let conn;
+
+    try {
+      conn = await oracledb.getConnection(config);
+
+      const result = await conn.execute(
+        'select * from "SMITHSTEPHEN"."ATHLETE_EVENT"'
+      );
+
+      console.log(result.rows);
+    } catch (err) {
+      console.log("Ouch!", err);
+    } finally {
+      if (conn) {
+        // conn assignment worked, need to close
+        await conn.close();
+      }
+    }
+  }
+
+  getAthletes();
+
+  return (
+    <div>
+      <h1>Welcome to the Olympics Database.</h1>
+      <p style={{ fontSize: "14px" }}>
+        This is a website that allows users to browse data analytics about the
+        past olympic games
+      </p>
+      <img src={olym} width="400" height="200" style={{ marginLeft: "50px" }} />
+    </div>
+  );
 };
 
 export default About;
- {/*width="300" height="200"*/ }
+{
+  /*width="300" height="200"*/
+}
