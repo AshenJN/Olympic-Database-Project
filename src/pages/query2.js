2eb39221a4ddb3145befc0c868c8c77bcd94ea30
Yifan Zhu <zhujimbo@gmail.com>
2023-04-20 00:12:10 -0400

    update

--- src/pages/query2.js
+++ src/pages/query2.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import i2 from './images/q2.png'
 class query2 extends React.Component {
 render(){
 	return (
@@ -15,7 +15,7 @@ render(){
 	 general status of a country.<br /> With a growing economy, a country can invest more in such events and use the scores of its 
 	 athletes to peacefully show its existence in the world, and vice versa. <br />
 	 Steadily growing scores of medals over a certain time period usually reflect steady development of the country within this time.
-
+	 <br /><img src={i2} alt="Excel trend for query 2." width="700" height="450"/>
 	</div>
 	
 )
--- src/pages/query5.js
+++ src/pages/query5.js
@@ -1,20 +1,23 @@
 import React from 'react';
-
+import i5 from './images/q5.png'
 class query5 extends React.Component {
 render(){
 	return (
 	<div>
-	<h1>Query5</h1>
-
-	<p style={{fontSize: "14px"}}>
-	Query 5 shows the trend of how BMI affects different sports of athletes in summer and winter Olympics.</p>
-
-	Different sports have different optimal body situations for athletes, so the athletes participating in different 
-	sports and different seasons may have significantly different trends in their BMI. <br />
-	This trend can list out different sports and the corresponding average BMI of the athletes to see athletes in which
-	 sports have significantly different BMI compared to others. 
+	<h1>Query 5: The trend of the greatest number of medals won by a team at the Olympics every year</h1>
 
 
+
+	<br />This graph shows the greatest number of medals won by a team each year of the Olympics. Since the very first Olympic games, 
+	<br />the team that takes home the greatest number of medals has stayed consistent with the USA brining the greatest number of medals the most times. 
+	<br />In this graph, the teams do not really change with teams such as the Soviet Union, Canada, and Germany appearing multiple times. 
+	<br />This graph clearly shows the amount of money each country has invested in their Olympic teams and gives a clear picture on what the world's economy looked like every year. 
+	<br />In the years 1960 to 1988, the Soviet Union was taking home the greatest number of medals, which lines up with the fact that at this time they were investing their money on their athletes to prove they were the best in the world.
+	<br /> However, when their country collapsed in 1991, they never won the greatest number of medals again, the USA winning the most. 
+	<br /> Due to recessions in the economy, the greatest number of medals taken home fluctuates between 50 and 250 in the years 1994 to 2016.
+	<br />
+	<br />
+	<img src={i5} alt="Excel trend for query 5." width="660" height="400"/>
 	</div>
 	
 )
